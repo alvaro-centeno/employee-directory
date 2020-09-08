0bc0cc80e6069e3fd3dac4754a5f963a5e028d70
Alvaro Centeno <alvaroc0689@gmail.com>
2020-09-08 13:47:39 -0700

    Updates

---
+++ precache-manifest.edb7a0c88e72fda24debb8d01a245610.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2f5073ca934e7a8ffc7cd50b6f002c",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "7263bb70ca16eabdd56f",
+    "url": "/employee-directory/static/css/main.97cf21fe.chunk.css"
+  },
+  {
+    "revision": "b8c42f40906a9bb88a49",
+    "url": "/employee-directory/static/js/2.46409222.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/employee-directory/static/js/2.46409222.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7263bb70ca16eabdd56f",
+    "url": "/employee-directory/static/js/main.c5321baf.chunk.js"
+  },
+  {
+    "revision": "87b30ff774bbf7d88de3",
+    "url": "/employee-directory/static/js/runtime-main.23cae75f.js"
+  }
+]);
