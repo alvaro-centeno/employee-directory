0f051937052c4bc09b05100338cb58554f8edbba
Alvaro Centeno <alvaroc0689@gmail.com>
2020-09-08 13:19:04 -0700

    created columns and names

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
-import React from 'react'
+mport React from "react";
+import "../style/Style.css";
+import Column from "./Column";
 
-function Header(props) {
-    const { names } = props;
-    const headers = names.map(name => <th scope='col'>{name}</th>);
-
+function Header() {
+    const columns = require("../database/columnNames");;
     return (
-        <thread>
-            <tr>{headers}</tr>
-        </thread>
+        <thead className=" ">
+            <tr >
+                {columns.map(column => (
+                    <Column column={column} />
+                ))}
+            </tr>
+        </thead>
     )
-}
+};
 
-export default Header
+export default Header;
+
---
+++ src/database/columnNames.js
@@ -0,0 +1,37 @@
+module.exports = (
+    [
+        {
+            colname: "name",
+            colDes: " Employee"
+        },
+        {
+            colname: "jobTitle",
+            colDes: "Job Title"
+        },
+        {
+            colname: "department",
+            colDes: "Department"
+        },
+        {
+            colname: "empStatus",
+            colDes: "Employment Status"
+        },
+        {
+            colname: "empType",
+            colDes: "Employee Type"
+        },
+        {
+            colname: "stdHours",
+            colDes: "Standard Hours"
+        },
+        {
+            colname: "ansal",
+            colDes: "Annual Salary"
+        },
+        {
+            colname: "hRate",
+            colDes: "Hourly Rate"
+        }
+    ]
+);
+
